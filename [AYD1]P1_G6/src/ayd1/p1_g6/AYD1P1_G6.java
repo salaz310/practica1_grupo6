8a710a8336c3aa9dad2c09bd9e68810c864b1f65
salaz310 <lrso.10@hotmail.com>
2018-08-22 16:19:10 -0500

    Añadiendo la interfaz

---
+++ [AYD1]P1_G6/src/ayd1/p1_g6/AYD1P1_G6.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ayd1.p1_g6;
+
+/**
+ *
+ * @author Sistemas
+ */
+public class AYD1P1_G6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        calculadora calc = new calculadora();
+        calc.setVisible(true);
+        calc.setLocationRelativeTo(null);
+    }
+    
+}
