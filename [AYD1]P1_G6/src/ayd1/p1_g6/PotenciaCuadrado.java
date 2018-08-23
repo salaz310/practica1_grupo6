121d5ff58470387fdeadf158cbac420ef106a482
Diego Momotic <diegomomotic@gmail.com>
2018-08-23 09:51:48 -0600

    Creacion de clase potencia cuadrado - 201318633

---
+++ [AYD1]P1_G6/src/ayd1/p1_g6/PotenciaCuadrado.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ayd1.p1_g6;
+
+/**
+ *
+ * @author damon
+ */
+public class PotenciaCuadrado {
+    public int getPotenciaCuadrado(int num){
+        return num * num;
+    }
+}
